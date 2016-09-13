5fd2ead141da49436a9783cfa0d6913ea5dfd766
Emman <emmanuel.goh.7@gmail.com>
2016-09-14 00:19:40 +0800

    Update workflow for gulp and npm

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd /var/www/wishpool-3216.github.io
-kill $(lsof -ti :4000)
+forever stopall
 export GIT_MERGE_AUTOEDIT=no
 git fetch
 git pull
+sudo npm install
 forever start node_modules/gulp/bin/gulp.js
