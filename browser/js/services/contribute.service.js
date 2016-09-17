5612914f6e109c423ffc649479773a1eafa543fd
superburrito <chuayaohui@gmail.com>
2016-09-18 00:26:32 +0800

    added contribute service

---
+++ browser/js/services/contribute.service.js
@@ -0,0 +1,50 @@
+'use strict';
+
+app.factory('ContributeService', function($http, __env){
+	var ContributeService = {};
+
+	// sUrl refers to server URL
+	var sUrl = __env.apiUrl;
+	var getResponseData = function(response){
+		return response.data;
+	}
+
+
+	// Gets all contributions associated with a gift
+	ContributeService.getContributions = function(giftId){
+		$http.get(sUrl + '/api/v1/gifts/' + giftId + '/contributions')
+		.then(getResponseData);
+	}
+
+
+	// Gets a single contribution, requires contrId
+	ContributeService.getContribution = function(contrId){
+		$http.get(sUrl + '/api/v1/contributions/' + contrId)
+		.then(getResponseData);
+	}
+
+
+	// Adds a contribution to a gift, requires giftId and amt
+	ContributeService.addContribution = function(giftId, amount){
+		$http.post(sUrl + '/api/v1/gifts/' + giftId + '/contributions', {amount: amount})
+		.then(getResponseData);
+	}
+
+
+	// Updates a contribution, requires contrId and amt
+	ContributeService.updateContribution = function(contrId, amount){
+		$http.patch(sUrl + '/api/v1/contributions/' + contrId, {amount: amount})
+		.then(getResponseData);
+	}
+
+
+	// Deletes a contribution, requires contrId
+	ContributeService.deleteContribution = function(contrId){
+		$http.delete(sUrl + '/api/v1/contributions/' + contrId)
+		.then(getResponseData);
+	}
+
+
+	return ContributeService;
+
+})
