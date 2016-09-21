50748d196cfdd58ac10bc2b7a9f2364a0eb4a9ea
buidohiep <buidohiep@gmail.com>
2016-09-22 04:02:51 +0800

    add cache service implement cache for friend list

---
+++ browser/js/services/cache.service.js
@@ -0,0 +1,32 @@
+
+app.factory('WishPoolCacheService', function($cacheFactory, $q) {
+  var WishPoolCacheService = {};
+
+  WishPoolCacheService.createCache = function(name) {
+    return $cacheFactory(name || 'defaultCache');
+  }
+
+  WishPoolCacheService.put = function(cache, key, value) {
+    cache.put(key, value);
+  }
+
+  WishPoolCacheService.isInCache = function(cache, key) {
+    return cache.get(key) != null;
+  }
+
+  WishPoolCacheService.requestCache = function(cache, key) {
+    return $q(function(resolve) {
+      resolve(cache.get(key))
+    });
+  }
+
+  WishPoolCacheService.cacheData = function(cache, key) {
+		return function(response) {
+			WishPoolCacheService.put(cache, key, response);
+			return response;
+		}
+	}
+
+
+  return WishPoolCacheService;
+});
--- browser/js/services/user.service.js
+++ browser/js/services/user.service.js
@@ -1,10 +1,12 @@
 'use strict';
 
-app.factory('UserService', function($http, __env, $stateParams, $rootScope, LocalStorageService){
+app.factory('UserService', function($http, $stateParams, WishPoolCacheService, LocalStorageService, __env){
 	var UserService = {};
 
 	// sUrl refers to server URL
 	var sUrl = __env.apiUrl;
+	var cache = WishPoolCacheService.createCache('userCache');
+
 	var getResponseData = function(response){
 		return response.data;
 	}
@@ -24,9 +26,11 @@ app.factory('UserService', function($http, __env, $stateParams, $rootScope, Loca
 
 
   // Gets user's friends (and birthdays) in an array
-  UserService.getUserFriends = function(userId){
+  UserService.getUserFriends = function(userId) {
+		var key = 'user_friends';
+		if (WishPoolCacheService.isInCache(cache, key)) return WishPoolCacheService.requestCache(cache, key);
   	return $http.get(sUrl + '/api/v1/users/' + userId + '/friend_birthdays')
-  	.then(getResponseData);
+  	.then(getResponseData).then(WishPoolCacheService.cacheData(cache, key));
   }
 
 	return UserService;
