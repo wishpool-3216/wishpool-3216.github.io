6139e73374fe0af222b07c33463bf48f5c65df21
buidohiep <buidohiep@gmail.com>
2016-09-21 18:37:03 +0800

    add thumnail image filter

--- browser/js/filters/filter.js
+++ browser/js/filters/filter.js
@@ -10,3 +10,14 @@ app.filter('defaultPhoto', function () {
     return url || 'https://www.us.aspjj.com/sites/aspjj.com.us/files/default_images/No_available_image_3.jpg';
   };
 });
+
+app.filter('defaultPhotoThumnail', function () {
+  return function (url) {
+    if (url) {
+      var params = url.split('/');
+      params.splice(params.length - 2, 0, 'w_100,h_100');
+      url = params.join('/');
+    }
+    return url || 'https://www.us.aspjj.com/sites/aspjj.com.us/files/default_images/No_available_image_3.jpg';
+  };
+});
