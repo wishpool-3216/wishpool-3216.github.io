0d72bdf61f4f5a1237bb0b126715539a55d7a010
superburrito <chuayaohui@gmail.com>
2016-09-23 14:50:00 +0800

    service worker only fetches GET requests

--- browser/service-worker.js
+++ browser/service-worker.js
@@ -60,6 +60,9 @@ self.addEventListener('activate', function (event) {
 // Fetching
 self.addEventListener('fetch', function (event) {
   var apiUrl = 'https://server.wishpool.info/api/v1/';
+  // First ensure that the request is a GET
+  if(event.request.method !== "GET") return;
+
   // If an API request was made
   if (event.request.url.indexOf(apiUrl) === 0) {
     event.respondWith(
